451a2a52bcd72c0990ca4518c7fdea1805b98f90
hutton85is <hutton85is@yahoo.com>
2018-04-05 20:28:46 +0000

    added true false and notequals function

--- include/JRTest.h
+++ include/JRTest.h
@@ -7,43 +7,110 @@ using namespace std;
 
 class JRTest
 {
-    public:
-        JRTest()
+public:
+
+    /**
+        Initialize all counter variables
+    */
+    JRTest()
+    {
+        test_counter = 0;
+        test_failed = 0;
+        test_passed = 0;
+    };
+
+    /**
+        When testing is done the de-constructor is run to do a summary
+    */
+    ~JRTest()
+    {
+        cout << endl;
+        cout << "* * * SUMMARY * * *" << endl;
+        cout << test_failed << " Failed" << endl;
+        cout << test_passed << " Passed" << endl;
+        cout << test_counter << " Total Test's" << endl;
+    };
+
+
+    /**
+        Test if variable T1 equals T2
+    */
+    template<class T1, class T2>
+    void equals(T1 a, T2 b)
+    {
+        test_counter++;
+
+        if (a != b)
         {
-            test_counter = 0;
-            test_failed = 0;
-            test_passed = 0;
-        };
-
-        ~JRTest()
+            cout << "Test no. '" << test_counter << "' failed" << endl;
+            test_failed++;
+        }
+        else
         {
-            cout << endl;
-            cout << "* * * SUMMARY * * *" << endl;
-            cout << test_failed << " Failed" << endl;
-            cout << test_passed << " Passed" << endl;
-            cout << test_counter << " Total Test's" << endl;
-        };
+            test_passed++;
+        }
+    };
 
-        template<class T1, class T2>
-        void equals(T1 a, T2 b)
+    /**
+        Test if variable T1 does not equal T2
+    */
+    template<class T1, class T2>
+    void notequal(T1 a, T2 b)
+    {
+        test_counter++;
+
+        if (a == b)
         {
-            test_counter++;
+            cout << "Test no. '" << test_counter << "' failed" << endl;
+            test_failed++;
+        }
+        else
+        {
+            test_passed++;
+        }
+    };
 
-            if (a != b)
-            {
-                cout << "Test no. '" << test_counter << "' failed" << endl;
-                test_failed++;
-            }
-            else
-            {
-                test_passed++;
-            }
-        };
+    /**
+        Test if statement is true
+    */
+    void istrue(bool a)
+    {
+        test_counter++;
 
-    private:
-        int test_counter;
-        int test_failed;
-        int test_passed;
+        if (!a)
+        {
+            cout << "Test no. '" << test_counter << "' failed" << endl;
+            test_failed++;
+        }
+        else
+        {
+            test_passed++;
+        }
+    };
+
+    /**
+        Test if statement is false
+    */
+    void isfalse(bool a)
+    {
+        test_counter++;
+
+        if (a)
+        {
+            cout << "Test no. '" << test_counter << "' failed" << endl;
+            test_failed++;
+        }
+        else
+        {
+            test_passed++;
+        }
+    };
+
+private:
+
+    unsigned int test_counter;
+    unsigned int test_failed;
+    unsigned int test_passed;
 };
 
 #endif // JRTEST_H
--- main.cpp
+++ main.cpp
@@ -6,15 +6,64 @@ using namespace std;
 int main()
 {
     JRTest test;
-    int a = 1;
-    int b = 2;
-    test.equals(a,b);
+
+    /**
+        Test 'equals' function
+    */
+    // test 1
     test.equals(1,4);
+    // test 2
     test.equals(2,2);
+    // test 3
     test.equals("russell","russell");
+    // test 4
     test.equals("russell","russ");
+    // test 5
     test.equals('s','a');
+    // test 6
     test.equals("a","a");
+    // test 7
     test.equals('a','a');
+
+    /**
+        Test 'notequals' function
+    */
+    // test 8
+    test.notequal('a', 'a');
+    // test 9
+    test.notequal('b','a');
+    // test 10
+    test.notequal(1,1);
+    // test 11
+    test.notequal(1,-733);
+    // test 12
+    test.notequal("Hutton", "hutton");
+    // test 13
+    test.notequal("JRHutton", "JRHutton");
+
+    /**
+        Test 'istrue' function
+    */
+    // test 14
+    test.istrue(1);
+    // test 15
+    test.istrue(0);
+    // test 16
+    test.istrue('a' == 'a');
+    //test 17
+    test.istrue("mama" == "papa");
+
+    /**
+        Test 'isfalse' function
+    */
+    // test 18
+    test.isfalse(1);
+    // test 19
+    test.isfalse(0);
+    // test 20
+    test.isfalse('a' == 'a');
+    //test 21
+    test.isfalse("mama" == "papa");
+
     return 0;
 }
